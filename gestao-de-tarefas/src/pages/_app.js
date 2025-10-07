c571ce458210235a47018c0f1a30958e46bdcb05
gonçalo <goncalon2107@gmail.com>
2025-10-07 16:19:22 +0100

    bg black

--- gestao-de-tarefas/src/pages/_app.js
+++ gestao-de-tarefas/src/pages/_app.js
@@ -4,7 +4,7 @@ import Footer from "@/components/Footer";
 
 export default function App({ Component, pageProps }) {
   return (
-    <div className="min-h-screen bg-gray-50 flex flex-col">
+    <div className="min-h-screen bg-black-50 flex flex-col">
       <Navbar />
       <main className="container mx-auto px-4 py-8 flex-grow">
         <Component {...pageProps} />
--- gestao-de-tarefas/src/pages/index.js
+++ gestao-de-tarefas/src/pages/index.js
@@ -3,7 +3,7 @@ import Link from 'next/link';
 export default function Home() {
   return (
     <div className="text-center">
-      <h1 className="text-4xl font-bold text-gray-800 mb-6">
+      <h1 className="text-4xl font-bold text-gray-800 mb-6 ">
         Bem-vindo à Nossa Loja
       </h1>
       <p className="text-xl text-gray-600 mb-8">
